94f5520abe7a94958ceb0a2e2e0a337a49e16d4c
zmoon <zmoon92@gmail.com>
2025-03-07 12:41:45 -0600

    More discovery options

--- monetio/obs/openaq_aws.py
+++ monetio/obs/openaq_aws.py
@@ -69,12 +69,22 @@ def read(fp):
     return df
 
 
-def _maybe_to_list(x):
+def _maybe_to_list(x, *, not_none=False):
     """Convert non-None scalar to singleton list, or return original."""
+    if not_none:
+        assert x is not None
     if x is not None and pd.api.types.is_scalar(x):
         return [x]
     else:
         return x
+
+
+def _to_datetime_index(dates, **kwargs):
+    dates = pd.to_datetime(dates, **kwargs)
+    if pd.api.types.is_scalar(dates):
+        dates = pd.DatetimeIndex([dates])
+
+    return dates
 
 
 def _cache_site_days():
@@ -113,12 +123,13 @@ def get_paths(dates, *, siteid=None, country=None, provider=None):
     """
     Parameters
     ----------
-    dates : pd.DatetimeIndex
+    dates : datetime-like or array-like of datetime-like
     """
     import s3fs
 
     fs = s3fs.S3FileSystem(anon=True)
 
+    dates = _to_datetime_index(dates)
     location_ids = _maybe_to_list(siteid)
     providers = _maybe_to_list(provider)
     countries = _maybe_to_list(country)
@@ -158,13 +169,13 @@ def get_paths(dates, *, siteid=None, country=None, provider=None):
     if providers is not None:
         tpl = (
             "openaq-data-archive/records/csv.gz/"
-            "provider={prvdr}/country={cntry}/locationid={loc}/"
+            "provider={prvdr}/country=*/locationid={loc}/"
             "year={date:%Y}/month={date:%m}/"
             "location-{loc}-{date:%Y%m%d}.csv.gz"
         )
         for date in unique_dates:
             for prvdr in providers:
-                glb = tpl.format(prvdr=prvdr.lower(), cntry="*", loc="*", date=date)
+                glb = tpl.format(prvdr=prvdr.lower(), loc="*", date=date)
                 prvdr_date_paths = fs.glob(glb)
                 logger.debug(f"found {len(prvdr_date_paths)} path(s) for glob='{glb}'")
                 paths.extend(prvdr_date_paths)
@@ -172,7 +183,7 @@ def get_paths(dates, *, siteid=None, country=None, provider=None):
     if countries is not None:
         tpl = (
             "openaq-data-archive/records/csv.gz/"
-            "country={cntry}/locationid={loc}/"
+            "provider=*/country={cntry}/locationid={loc}/"
             "year={date:%Y}/month={date:%m}/"
             "location-{loc}-{date:%Y%m%d}.csv.gz"
         )
@@ -187,10 +198,73 @@ def get_paths(dates, *, siteid=None, country=None, provider=None):
     return sorted(set(paths))
 
 
-def get_locations(*, country=None, provider=None):
-    """Get location IDs corresponding to country/countries OR provider(s).
+def get_providers():
+    """Get OpenAQ data providers by searching the bucket paths.
 
-    Default: all locations.
+    As such, these are just lowercase short names.
+
+    Returns
+    -------
+    list of str
+    """
+    import s3fs
+
+    fs = s3fs.S3FileSystem(anon=True)
+
+    paths = fs.glob("openaq-data-archive/records/csv.gz/provider=*", maxdepth=1)
+    logger.debug(f"found {len(paths)} paths")
+
+    providers = []
+    for p in paths:
+        _, provider = p.split("=")
+        providers.append(provider)
+
+    logger.debug(f"found {len(providers)} providers")
+    if not providers:
+        warnings.warn("no providers found", stacklevel=2)
+
+    return providers
+
+
+def get_provider_countries(provider):
+    """Get countries for a given provider by searching the bucket paths.
+
+    Parameters
+    ----------
+    provider : str
+        OpenAQ data provider lowercase short name, e.g. 'airnow', 'aqdc'.
+
+    Returns
+    -------
+    list of str
+    """
+    import s3fs
+
+    fs = s3fs.S3FileSystem(anon=True)
+
+    glb = f"openaq-data-archive/records/csv.gz/provider={provider.lower()}/country=*"
+    paths = fs.glob(glb, maxdepth=1)
+    logger.debug(f"found {len(paths)} paths for provider={provider!r}")
+
+    countries = []
+    for p in paths:
+        _, _, country = p.split("=")
+        countries.append(country)
+
+    logger.debug(f"found {len(countries)} countries for provider={provider!r}")
+    if not countries:
+        warnings.warn(f"no countries found for provider={provider!r}", stacklevel=2)
+
+    return countries
+
+
+def get_all_locations():
+    """Get all location IDs by searching for /records/csv.gz/locationid=* bucket paths.
+
+    .. note::
+       This returns a significantly greater number of location IDs
+       than using :func:`get_locations` with ``provider=None`` and ``country=None``.
+       The latter's count is more consistent with results from the web API.
 
     Returns
     -------
@@ -200,72 +274,94 @@ def get_locations(*, country=None, provider=None):
 
     import s3fs
 
-    if country is not None and provider is not None:
-        raise ValueError("cannot specify both `country` and `provider`")
+    print("discovering locations...")
+    fs = s3fs.S3FileSystem(anon=True)
+
+    # Note: ~ 2x faster than using fs.glob
+    paths = fs.find("openaq-data-archive/records/csv.gz/", withdirs=True, maxdepth=1)
+    logger.debug(f"found {len(paths)} paths")
+
+    locs = []
+    for p in paths:
+        m = re.fullmatch(r"openaq-data-archive/records/csv\.gz/locationid=([0-9]+)", p)
+        if m is not None:
+            locs.append(m.group(1))
+
+    logger.debug(f"found {len(locs)} locations")
+    if not locs:
+        warnings.warn("no locations found", stacklevel=2)
+
+    return sorted(locs)
+
+
+def get_locations(*, provider=None, country=None):
+    """Get location IDs corresponding to provider(s) and/or country(ies)
+    by searching for /records/csv.gz/provider=*/country=*/locationid=* bucket paths.
+
+    Default: all locations (search all providers/countries;
+    should take less than 10 seconds).
+
+    Returns
+    -------
+    list of str
+    """
+    import re
+
+    import s3fs
+
+    if country is None and provider is None:
+        warnings.warn(
+            "get_all_locations() is a faster way to get all locations",
+            stacklevel=2,
+        )
 
     print("discovering locations...")
     fs = s3fs.S3FileSystem(anon=True)
-    if country is not None:
-        if pd.api.types.is_scalar(country):
-            countries = [country]
+
+    country = _maybe_to_list(country)
+    if provider is None:
+        providers = get_providers()
+    else:
+        providers = _maybe_to_list(provider, not_none=True)
+
+    paths = []
+    for prvdr in providers:
+        if country is None:
+            countries = get_provider_countries(prvdr)
         else:
             countries = country
 
-        paths = []
         for cntry in countries:
-            cntry_paths = fs.find(
-                # TODO: these paths (csv.gz/country=) are no longer there
-                f"openaq-data-archive/records/csv.gz/country={cntry.lower()}/",
+            glb = (
+                "openaq-data-archive/records/csv.gz/"
+                f"provider={prvdr.lower()}/country={cntry.lower()}/"
+            )
+            prvdr_cntry_paths = fs.find(
+                glb,
                 withdirs=True,
                 maxdepth=1,
             )
-            paths.extend(cntry_paths)
+            paths.extend(prvdr_cntry_paths)
 
-        locs = []
-        for p in paths:
-            m = re.fullmatch(
-                r"openaq-data-archive/records/csv\.gz/country=([a-z]{2}|\-\-|99)/"
-                r"locationid=([0-9]+)",
-                p,
-            )
-            if m is not None:
-                locs.append(m.group(2))
+    logger.debug(f"found {len(paths)} paths")
 
-    elif provider is not None:
-        if pd.api.types.is_scalar(provider):
-            providers = [provider]
-        else:
-            providers = provider
+    locs = []
+    for p in paths:
+        m = re.fullmatch(
+            r"openaq-data-archive/records/csv\.gz/"
+            r"provider=([a-z0-9\-]+)/country=([a-z]{2}|\-\-|99|mobile)/"
+            r"locationid=([0-9]+)",
+            p,
+        )
+        if m is not None:
+            locs.append(m.group(3))
 
-        paths = []
-        for prvdr in providers:
-            prvdr_paths = fs.find(
-                f"openaq-data-archive/records/csv.gz/provider={prvdr.lower()}/",
-                withdirs=True,
-                maxdepth=2,
-            )
-            paths.extend(prvdr_paths)
-
-        locs = []
-        for p in paths:
-            m = re.fullmatch(
-                r"openaq-data-archive/records/csv\.gz/provider=([a-z0-9\-]+)/"
-                r"country=([a-z]{2}|\-\-|99)/locationid=([0-9]+)",
-                p,
-            )
-            if m is not None:
-                locs.append(m.group(3))
-
-    else:  # All locs
-        paths = fs.find("openaq-data-archive/records/csv.gz/", withdirs=True, maxdepth=1)
-        locs = []
-        for p in paths:
-            m = re.fullmatch(r"openaq-data-archive/records/csv\.gz/locationid=([0-9]+)", p)
-            if m is not None:
-                locs.append(m.group(1))
-
+    logger.debug(f"found {len(locs)} locations")
     if not locs:
-        warnings.warn(f"no locations found for country={country!r} provider={provider!r}")
+        warnings.warn(
+            f"no locations found for country={country!r} provider={provider!r}",
+            stacklevel=2,
+        )
 
     return sorted(locs)
 
@@ -289,9 +385,7 @@ def _build_urls(dates, sites, *, protocol="s3"):
     -------
     list of str
     """
-    dates = pd.to_datetime(dates)
-    if pd.api.types.is_scalar(dates):
-        dates = pd.DatetimeIndex([dates])
+    dates = _to_datetime_index(dates)
 
     if pd.api.types.is_scalar(sites):
         sites = [sites]
@@ -355,6 +449,8 @@ def add_data(
         For example, from :func:`get_locations`.
     country : str or list of str, optional
         Country or countries to include. 2-character ISO country codes.
+        'mobile' is also an option.
+        Other special values are '99' and '--'.
     provider : str or list of str, optional
         Data provider(s) to include.
 
